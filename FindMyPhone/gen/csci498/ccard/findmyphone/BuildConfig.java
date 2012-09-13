1bde5de3bfa451b5bcb444f4353e3cb23d1990c2
Chris Card <ccard@mymail.mines.edu>
2012-09-13 13:14:33 -0600

    Added readme file and had initial commints to be commited

---
+++ FindMyPhone/gen/csci498/ccard/findmyphone/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package csci498.ccard.findmyphone;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+README
+
+Authors: Christopher Card
+		 Gurpreet Nanda
+		 Tony Nyugen
+
+This driectory contains the term project for csci 498 named find my phone
