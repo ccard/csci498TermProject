6838c26349822eeb7693a75a69c2aab496978226
Chris Card <ccard@mymail.mines.edu>
2012-09-13 17:50:19 -0600

    added a background to the login screen and supporting code and files

--- FindMyPhone/gen/csci498/ccard/findmyphone/R.java
+++ FindMyPhone/gen/csci498/ccard/findmyphone/R.java
@@ -16,47 +16,49 @@ public final class R {
         public static final int droppin=0x7f020002;
         public static final int ic_action_search=0x7f020003;
         public static final int ic_launcher=0x7f020004;
-        public static final int login_button=0x7f020005;
-        public static final int messagebackground=0x7f020006;
-        public static final int phoneicon=0x7f020007;
-        public static final int roundedboarders=0x7f020008;
+        public static final int login_background=0x7f020005;
+        public static final int login_button=0x7f020006;
+        public static final int messagebackground=0x7f020007;
+        public static final int phoneicon=0x7f020008;
+        public static final int roundedboarders=0x7f020009;
     }
     public static final class id {
-        public static final int MyPhones=0x7f080014;
-        public static final int backphone=0x7f08001a;
-        public static final int backsend=0x7f08001e;
-        public static final int createAccount=0x7f080009;
-        public static final int createNewAccount=0x7f080012;
-        public static final int createTitle=0x7f08000a;
-        public static final int email=0x7f080005;
-        public static final int emailtitle=0x7f08000d;
-        public static final int emaltitle=0x7f080004;
-        public static final int icon=0x7f080015;
-        public static final int linearLayout1=0x7f080002;
-        public static final int login=0x7f080008;
-        public static final int logintitle=0x7f080003;
+        public static final int MyPhones=0x7f080015;
+        public static final int backphone=0x7f08001b;
+        public static final int backsend=0x7f08001f;
+        public static final int createAccount=0x7f08000a;
+        public static final int createNewAccount=0x7f080013;
+        public static final int createTitle=0x7f08000b;
+        public static final int email=0x7f080006;
+        public static final int emailtitle=0x7f08000e;
+        public static final int emaltitle=0x7f080005;
+        public static final int icon=0x7f080016;
+        public static final int linearLayout1=0x7f080003;
+        public static final int login=0x7f080009;
+        public static final int logintitle=0x7f080004;
         public static final int mapview=0x7f080000;
-        public static final int menu_settings=0x7f080022;
-        public static final int message=0x7f08001c;
-        public static final int name=0x7f08000c;
-        public static final int nametitle=0x7f08000b;
-        public static final int pass=0x7f080006;
-        public static final int password=0x7f080007;
-        public static final int password1=0x7f08000f;
-        public static final int passwordConfirm=0x7f080011;
-        public static final int passwrod1title=0x7f08000e;
-        public static final int passwrodConfirmTitle=0x7f080010;
-        public static final int phoneName=0x7f080020;
-        public static final int phoneType=0x7f080021;
-        public static final int phoneicon=0x7f08001f;
-        public static final int phonename=0x7f080016;
-        public static final int playtone=0x7f080019;
-        public static final int scroll=0x7f080001;
-        public static final int send=0x7f08001d;
-        public static final int sendMessagetitle=0x7f08001b;
-        public static final int sendamessage=0x7f080017;
-        public static final int showphone=0x7f080018;
-        public static final int viewFlipperMyDevice=0x7f080013;
+        public static final int menu_settings=0x7f080023;
+        public static final int message=0x7f08001d;
+        public static final int name=0x7f08000d;
+        public static final int nametitle=0x7f08000c;
+        public static final int pass=0x7f080007;
+        public static final int password=0x7f080008;
+        public static final int password1=0x7f080010;
+        public static final int passwordConfirm=0x7f080012;
+        public static final int passwrod1title=0x7f08000f;
+        public static final int passwrodConfirmTitle=0x7f080011;
+        public static final int phoneName=0x7f080021;
+        public static final int phoneType=0x7f080022;
+        public static final int phoneicon=0x7f080020;
+        public static final int phonename=0x7f080017;
+        public static final int playtone=0x7f08001a;
+        public static final int scroll=0x7f080002;
+        public static final int scrollback=0x7f080001;
+        public static final int send=0x7f08001e;
+        public static final int sendMessagetitle=0x7f08001c;
+        public static final int sendamessage=0x7f080018;
+        public static final int showphone=0x7f080019;
+        public static final int viewFlipperMyDevice=0x7f080014;
     }
     public static final class layout {
         public static final int activity_display_map=0x7f030000;
@@ -86,6 +88,7 @@ public final class R {
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
+        public static final int LoginTheme=0x7f060002;
         public static final int MapTheme=0x7f060001;
     }
 }
