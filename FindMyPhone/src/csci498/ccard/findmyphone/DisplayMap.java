eb1951d137f1f321d936a7bc0fe414ee37725a59
Chris Card <ccard@mymail.mines.edu>
2012-09-19 10:49:14 -0600

    added accuracy circle around the drop pin for the location of the phone

--- FindMyPhone/src/csci498/ccard/findmyphone/DisplayMap.java
+++ FindMyPhone/src/csci498/ccard/findmyphone/DisplayMap.java
@@ -17,6 +17,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 
@@ -31,6 +32,7 @@ public class DisplayMap extends MapActivity {
 	
 	private GeoPoint loc;
 	private MapController mc;
+	private float gpsAccuracy;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,6 +58,7 @@ public class DisplayMap extends MapActivity {
         	loc = new GeoPoint(lat,lon);
         	mc.setCenter(loc);
         	mc.setZoom(14);
+        	gpsAccuracy = (float) 6.0;
         	MapOverlay overlay = new MapOverlay();
         	mOverlays.add(overlay);
         }
@@ -70,6 +73,7 @@ public class DisplayMap extends MapActivity {
 				int latE6 = (int)(location.getLatitude()*1E6);
 				int lonE6 = (int)(location.getLongitude()*1E6);
 			
+				gpsAccuracy = location.getAccuracy();
 				//put methods here to allow alternate phone to get this phones location
 				loc = new GeoPoint(latE6,lonE6);
 				mc.animateTo(loc);
@@ -104,6 +108,23 @@ public class DisplayMap extends MapActivity {
      */
 	public class MapOverlay extends Overlay {
 
+		private Paint paintBoarder;
+		private Paint fillPaint;
+
+		public MapOverlay()
+		{
+			super();
+
+			paintBoarder = new Paint();
+			paintBoarder.setStyle(Paint.Style.STROKE);
+			paintBoarder.setAntiAlias(true);
+			paintBoarder.setColor(0xee4D2EFF);
+			fillPaint = new Paint();
+			fillPaint.setStyle(Paint.Style.FILL);
+			fillPaint.setColor(0x154D2Eff);
+		}
+
+		//draw method for point and canvas
 		public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when)
 		{
 			super.draw(canvas, mapView, shadow);
@@ -111,7 +132,12 @@ public class DisplayMap extends MapActivity {
 			Point myScreen = new Point();
 			mapView.getProjection().toPixels(loc, myScreen);
 			Bitmap marker = BitmapFactory.decodeResource(getResources(), R.drawable.droppin);
-			canvas.drawBitmap(marker, myScreen.x, myScreen.y,null);
+			canvas.drawBitmap(marker, myScreen.x-20, myScreen.y-45,null);
+
+			int radius = (int)mapView.getProjection().metersToEquatorPixels(gpsAccuracy);
+
+			canvas.drawCircle(myScreen.x, myScreen.y, radius, paintBoarder);
+			canvas.drawCircle(myScreen.x, myScreen.y, radius, fillPaint);
 			return true;
 		}
 
