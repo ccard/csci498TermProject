65fd3c50ddd9faa8ba04480a35edd15705a68e90
Chris Card <ccard@mymail.mines.edu>
2012-10-29 14:09:39 -0600

    added on boot reciever

---
+++ FindMyPhone/src/csci498/ccard/findmyphone/OnBootReciever.java
@@ -0,0 +1,15 @@
+package csci498.ccard.findmyphone;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReciever extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
