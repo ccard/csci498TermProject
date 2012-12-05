f4cb3bb5b2cbc45155c3eaea9ac5656102e0d2ae
Chris Card <ccard@mymail.mines.edu>
2012-12-05 15:51:19 -0700

    This class is meant to be bound to another process so that it can communicate

---
+++ FindMyPhone/src/csci498/ccard/findmyphone/FunctionsSerivce.java
@@ -0,0 +1,23 @@
+/*
+ * Chris Card
+ * Gurpreet Nanda
+ * Tony Nguyen
+ * 12/5/12
+ * This service intened to be bound so we can get results from it
+ */
+
+package csci498.ccard.findmyphone;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class FunctionsSerivce extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
