752813e7896813aee6302071c81d677784a8afa6
Chris Card <ccard@mymail.mines.edu>
2012-12-05 15:56:54 -0700

    renamed functionsserivce to functionsservice

--- FindMyPhone/src/csci498/ccard/findmyphone/FunctionsService.java
+++ FindMyPhone/src/csci498/ccard/findmyphone/FunctionsService.java
@@ -10,14 +10,24 @@ package csci498.ccard.findmyphone;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
-public class FunctionsSerivce extends Service {
+public class FunctionsService extends Service {
 
 	@Override
 	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
+		
 		return null;
+	}
+	
+	
+	public class FunctionBinder extends Binder
+	{
+		FunctionsService getService()
+		{
+			
+		}
 	}
 
 }
